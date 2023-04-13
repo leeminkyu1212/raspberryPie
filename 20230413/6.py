592f33a1bda80a6ecb21991bad9f2ac4f80db67b
dkwl273 <dkwl273@naver.com>
2023-04-13 17:53:12 +0900

    20230413

--- 20230413/6.py
+++ 20230413/6.py
@@ -1,3 +1,21 @@
 # print(f"#{t} {max}")
 # print("#"+str(t)+" "+str(max))
 # 둘은 같다
+
+T=int(input())
+for t in range(1,T+1):
+    N, M = map(int, input().split())
+    matrix = [[0] * N for _ in range(N)]
+    for i in range(len(matrix)):
+        matrix[i] = list(map(int, input().split()))
+    dir = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    max = -10000
+    for row in range(N - M + 1):
+        for col in range(N - M + 1):
+            num = 0
+            for i in range(M):
+                for j in range(M):
+                    num+=matrix[row+i][col+j]
+            if (num > max): max = num
+
+    print(f"#{t} {max}")
